18a3b3fd858059d25653d3768e8f4c71d0f9058e
tophernk <topher.nk@googlemail.com>
2020-04-23 18:30:47 +0200

    move test

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,21 +6,22 @@ pipeline {
                 git 'https://github.com/tophernk/cchess.git'
             }
         }
-        stage('MAKE') {
+        stage('Make') {
             steps {
                 sh "cmake -DGCC=ON -DCLANG=OFF -j 4 ."
                 sh "make"
             }
         }
-        stage('TEST') {
+        stage('Test') {
             steps {
                 sh "ctest -VV"
             }
         }
-        stage('MEMORY') {
+        stage('Memory') {
             steps {
                 sh "valgrind --leak-check=full ./test/position_test"
                 sh "valgrind --leak-check=full ./test/piece_test"
+                sh "valgrind --leak-check=full ./test/move_test"
                 sh "valgrind --leak-check=full ./test/config_test"
             }
         }
---
+++ test/move_test.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "move.h"
+
+static void test_move_ctor(void **state) {
+    move_t *move = move_new();
+    move_ctor(move);
+
+    assert_false(position_valid(move_get_from_position(move)));
+    assert_false(position_valid(move_get_to_position(move)));
+    assert_true(move_get_piece_type(move) == NONE);
+
+    move_dtor(move);
+    free(move);
+}
+
+static void test_move_compare(void **state) {
+    move_t *a = move_new();
+    move_ctor(a);
+    move_t *b = move_new();
+    move_ctor(b);
+
+    move_t *as[1];
+    as[0] = a;
+    move_t *bs[1];
+    bs[0] = b;
+
+    assert_true(move_cmpr(as, bs, 1) == 0);
+
+    move_set_score(a, 1);
+    assert_true(move_cmpr(as, bs, 1) == 1);
+
+    move_dtor(b);
+    free(b);
+    move_dtor(a);
+    free(a);
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+            cmocka_unit_test(test_move_ctor),
+            cmocka_unit_test(test_move_compare)
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
