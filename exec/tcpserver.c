f38304ff0a787a9eeb1133fb39583d61c2982eb4
tophernk <topher.nk@googlemail.com>
2020-05-25 17:11:34 +0200

    tcp server

---
+++ exec/tcpserver.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <stream_server.h>
+
+int main(int argc, char **argv) {
+    // create server socket
+    int server_sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sd == -1) {
+        fprintf(stderr, "Could not create socket: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    // bind socket address
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(1024);
+
+    int result = bind(server_sd, (struct sockaddr *) &addr, sizeof(addr));
+    if (result == -1) {
+        close(server_sd);
+        fprintf(stderr, "Could not bind socket address: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    // listen with backlog
+    result = listen(server_sd, 10);
+    if (result == -1) {
+        close(server_sd);
+        fprintf(stderr, "listen for incoming connections failed: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    // accept connections
+    accept_forever(server_sd);
+
+    return 0;
+}
---
+++ server/stream_server.h
@@ -0,0 +1,6 @@
+#ifndef CCHESS_STREAM_SERVER_H
+#define CCHESS_STREAM_SERVER_H
+
+void accept_forever(int server_sd);
+
+#endif //CCHESS_STREAM_SERVER_H
